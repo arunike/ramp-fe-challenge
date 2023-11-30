9bb34c4cdd6c7afadfb10795c947ed84ebf38d71
Richie Zhou <r631137986@gmail.com>
2023-11-30 01:36:01 -0600

    removed node

--- node_modules/codesandbox/lib/api/define.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getParameters = void 0;
-var define_1 = require("codesandbox-import-utils/lib/api/define");
-exports.getParameters = define_1.getParameters;
-//# sourceMappingURL=define.js.map
